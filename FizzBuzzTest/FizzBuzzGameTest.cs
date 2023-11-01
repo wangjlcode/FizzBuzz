4eb6d2c4fde01afd9f1731b0387f4ac77209a0a2
Jinglin Wang <JWang317@slb.com>
2023-11-01 14:29:24 +0800

    test: add ut, given 3 multiple number, should return "Fizz"

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -15,5 +15,17 @@ namespace FizzBuzzTest
             //then
             Assert.Equal(result, number.ToString());
         }
+
+        [Fact]
+        public void Should_return_Fizz_given_3_multiple_number_when_CountOff()
+        {
+            //given
+            int number = 3;
+            //when
+            string result = FizzBuzzGame.CountOff(number);
+            //then
+            Assert.Equal(result, "Fizz");
+        }
+
     }
 }
