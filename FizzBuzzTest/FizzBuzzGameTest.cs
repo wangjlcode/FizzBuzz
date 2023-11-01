f8b302b864754b3d750d1633e4a0d3c9910a07cb
Jinglin Wang <JWang317@slb.com>
2023-11-01 14:17:44 +0800

    test: add ut for normal case

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -6,12 +6,14 @@ namespace FizzBuzzTest
     public class FizzBuzzGameTest
     {
         [Fact]
-        public void Test1()
+        public void Should_return_original_number_given_normal_case_number_when_CountOff()
         {
-            FizzBuzzGame game = new FizzBuzzGame();
-
+            //given
+            int number = 1;
+            //when
+            string result = FizzBuzzGame.CountOff(number);
             //then
-            Assert.NotNull(game);
+            Assert.Equal(result, number.ToString());
         }
     }
 }
