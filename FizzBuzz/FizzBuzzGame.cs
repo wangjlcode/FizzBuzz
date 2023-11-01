53e45a02abcfd3a424ab0a4532d04e0199dd8ddd
Jinglin Wang <JWang317@slb.com>
2023-11-01 14:31:14 +0800

    feat: implement return fizz given 3-multiple number

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -6,7 +6,14 @@ namespace FizzBuzz
     {
         public static string CountOff(int number)
         {
-            return number.ToString();
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return number.ToString();
+            }
         }
     }
 }
